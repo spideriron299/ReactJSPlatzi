90d4efe84d5120c336373473133a9e6672788f7f
Diego Badillo <diegobadillo@live.com>
2025-02-19 19:01:07 -0600

    Actualizacion de diseño finales

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,14 +1,17 @@
-import './TodoItems.css';
+import './TodoItem.css';
 
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-        <input type="image" src="/image/VPersona5.png" class="image-input" alt="Botón de aceptar" />
-        <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
+      <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}>
+        V
+      </span>
+      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <input type="image" src="/image/XPersona5.png" class="image-input" alt="Botón de imagen" />
-
+      <span className="Icon Icon-delete">
+        X
+      </span>
     </li>
   );
 }
