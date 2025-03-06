fcc49713c581a249c34a04be6e85f4b26329a0d9
Diego Badillo <diegobadillo@live.com>
2025-03-05 18:43:33 -0600

    Clase 7

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,14 +1,19 @@
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.
+  useState('');
+
+  console.log(searchValue);
+
   return (
     <input
       placeholder="Cortar cebolla"
       className="TodoSearch"
+      value={searchValue}
       onChange={(event) => {
-        console.log(event);
-        console.log(event.target);
-        console.log(event.target.value);
+        setSearchValue(event.target.value);
       }}
     />
   );
