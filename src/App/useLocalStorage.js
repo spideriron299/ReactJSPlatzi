42676309d6aeabfc217861cf9d1808ee0d3894db
Diego Badillo <diegobadillo@live.com>
2025-06-12 19:47:50 -0600

    Clase 21 Estados

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -7,15 +7,27 @@ function useLocalStorage(itemName, initialValue) {
   const [error, setError] = React.useState(false);
 
   React.useEffect(() => {
-    const localStorageItem = localStorage.getItem(itemName);
-    let parsedItem;
+    setTimeout(() => {
+      try {
+      const localStorageItem = localStorage.getItem(itemName);
+      let parsedItem;
 
-    if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
+      if (!localStorageItem) {
+      localStorage.setItem(itemName, JSON.stringify(initialValue));
+      parsedItem = initialValue;
+    } else {
+      parsedItem = JSON.parse(localStorageItem);
+      setItem(parsedItem);
+    }
+
+    setLoading(false);
+
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+    }, [ ]);
+
   });
 
   const saveItem = (newItem) => {
