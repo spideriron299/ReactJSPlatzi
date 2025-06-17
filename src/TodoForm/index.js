c72ac72711b99060e24300ac5c8cd02bfc55771d
Diego Badillo <diegobadillo@live.com>
2025-06-16 19:30:05 -0600

    Clase 27 App completada

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,24 +1,48 @@
 import React from "react";
+import { TodoContext } from '../TodoContext';
 import './TodoForm.css';
+
 function TodoForm(){
-    return(
-        <form onSubmit={(event) =>{
+    const {
+        addTodo,
+        setOpenModal,
+    } = React.useContext(TodoContext);
+
+    const [newTodoValue, setNewTodoValue] = React.useState('');
+
+    const onSubmit = (event) =>{
             event.preventDefault();
-        }}>
+            addTodo(newTodoValue);
+            setOpenModal(false);
+        };
+
+    const onCancel = (event) =>{
+            setOpenModal(false);
+        };
+
+    const onChange = (event) =>{
+            setNewTodoValue(event.target.value);
+        };
+
+    return(
+        <form onSubmit={onSubmit}>
             <label>Escribe tu nuevo ToDo</label>
             <textarea
                 placeholder="Escribe aqui tu ToDo"
-            />
+                value={newTodoValue}
+                onChange={onChange}
+                />
         <div className="TodoForm-buttonContainer">
             <button
-                className="TodoForm-button
-                TodoForm-button--cancel"
+            type="button"
+            className="TodoForm-button TodoForm-button--cancel"
+            onClick={onCancel}
             >Cancelar</button>
             <button
-                className="TodoForm-button
-                TodoForm-button--add"
+            type="submit"
+            className="TodoForm-button TodoForm-button--add"
             >Añadir</button>
-        </div>
+      </div>
         </form>
     );
 }
